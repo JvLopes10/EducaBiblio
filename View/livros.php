5c1789431622ef9cf21124b89fda4f513f3db6e5
karlarlima <karla.lima17@aluno.ce.gov.br>
2023-12-19 08:34:23 -0300

    Atualização 19.12

--- View/livros.php
+++ View/livros.php
@@ -190,11 +190,6 @@ if (!isset($_SESSION['usuario_logado']) || $_SESSION['usuario_logado'] !== true)
 							<option value="Sim">Material didático</option>
 							<option value="Não">Material não didático</option>
 						</select>
-
-<<<<<<< HEAD
-						<input type="file" name="FotoLivro" id="FotoLivro" class="box" required>
-=======
->>>>>>> 20a0816b6418686068f5d692017e5dc9345c900e
 						<input type="text" placeholder="Localização" name="LocalLivro" id="LocalLivro" class="box" autocomplete="off">
 						<select id="PrateleiraLivro" name="PrateleiraLivro" class="box select-dark-mode">
 							<option>Prateleira</option>
